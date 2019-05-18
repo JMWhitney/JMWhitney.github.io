212e3d33b3182c7ba92e069eda3e25e8742cdc97
JMWhitney <JMWhitney96@gmail.com>
2019-05-17 21:54:53 -0700

    Added animated background Added homepage text and styling Removed sass mapping

--- js/background.js
+++ js/background.js
@@ -1,11 +1,13 @@
-(function initBackground() {
-
+function initBackground() {
+  
   //Initialize canvas
   var container = document.getElementById('background');
   var canvas = container.appendChild(document.createElement('canvas'));
   canvas.height = container.clientHeight;
   canvas.width = container.clientWidth;
   ctx = canvas.getContext('2d');
+  var angle = -45 * Math.PI / 180;
+  ctx.rotate(angle);
   
   //Used to shade boxes
   var guassianDistribution = function(x, a, b, c){
@@ -14,41 +16,50 @@
     return a * Math.pow(Math.E, -1.0*((x-b)*(x-b))/(2*(c*c)));
   }
   
+  
   //Make sure canvas is resized properly
   function resize() {
     canvas.height = container.clientHeight;
     canvas.width = container.clientWidth;
   }
 
-  function drawBoxes() {
+  var running = false;
+  
+  function initAnimation() {
+    if(running) return;
+    running = true;
     //vars
-    var angle = -45 * Math.PI / 180;
     var hypotenuse = Math.round(Math.sqrt(container.clientWidth * container.clientWidth + container.clientHeight * container.clientHeight));
     var color = "#1d1d23";
     var boxWidth = 100;
     var boxHeight = window.innerHeight * 3;
     var numBoxes = Math.ceil(hypotenuse / boxWidth);
-    ctx.rotate(angle);
-    
-    //Draw array of boxes
-    for(let i = 0; i <= numBoxes; i++) {
-      ctx.fillStyle = color;
-      ctx.fillRect(( i * boxWidth ) - canvas.width/2, 0 , boxWidth - 2, boxHeight );
-      ctx.fillStyle = "#000000";
-      ctx.lineWidth = .2;
-      ctx.strokeRect(( i * boxWidth ) - canvas.width/2, 0 , boxWidth - 2, boxHeight );
-    }
-  }
-  drawBoxes();
-  
+    var n = -numBoxes;
+    var rms = 3;
+    var maxValue = 4;
 
-  function animateBoxes() {
-    //TODO
-    
-    setTimeout(5000, animateBoxes);
+    var frameLoop = setInterval(() => {
+      for(let i = 0; i <= numBoxes; i++) {
+        let r = 26 + guassianDistribution(i, maxValue, n , rms);
+        let g = 30 + guassianDistribution(i, maxValue, n , rms);
+        let b = 40 + guassianDistribution(i, maxValue, n , rms);
+        ctx.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+        ctx.fillRect(( i * boxWidth ) - canvas.width/2, 0 , boxWidth, boxHeight );
+      }
+      n++;
+      //Effect no longer visible. Stop animation.
+      if(n > numBoxes + rms*2 ) {
+        clearInterval(frameLoop);
+        running = false;
+      }
+    }, 100)
+
+    setTimeout(initAnimation, 6000);
   }
 
+  initAnimation();
 
   window.addEventListener('resize', resize);
-  window.addEventListener('resize', drawBoxes);
-})();
+  // window.addEventListener('resize', initAnimation);
+}
+initBackground();
