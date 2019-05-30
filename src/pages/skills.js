b5d2f547d7b9fdc01ea3b64881d9f38313b967e7
JMWhitney <JMWhitney96@gmail.com>
2019-05-29 23:22:34 -0700

    * Changed the appearance of the skills section.

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -5,8 +5,53 @@ import SEO from "../components/seo"
 const Skills = () => (
   <Layout>
     <SEO title="Justin Whitney" />
+
+    <div id="background">
+        <div className="grid-wrapper">
+          <div className="grid">
+            <div className="vertical">
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+            </div>
+            <div className="dots">
+              <div className="dot"></div>
+              <div className="dot"></div>
+              <div className="dot"></div>
+              <div className="dot"></div>
+              <div className="dot"></div>
+              <div className="dot"></div>
+              <div className="dot"></div>
+            </div>
+            <div className="horizontal">
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     <div id="skills">
-        <div className="tree">
+      
+      <div className="row">
+        <div className="tree card">
           <ul>
           <li>
             <button>Back-end</button>
@@ -42,83 +87,84 @@ const Skills = () => (
           </li>
         </ul>
       </div>
-
-      <div className="row">
-        <div className="tree">
-          <ul>
-            <li>
-              <button>Front-End</button>
-              <ul>
-
-                <li>
-                  <button>JavaScript (ES6+)</button>
-
-                    <ul>
-                      <li>
-                        <button>React.js</button>
-                      </li>
-                    </ul>
-                </li>
-
-                <li>
-                  <button>HTML5</button>
-
-                  <ul>
-                    <li>
-                      <button>Canvas</button>
-                    </li>
-                  </ul>
-
-                </li>
-
-                <li>
-                  <button>CSS3</button>
-                  <ul>
-
-                    <li>
-                      <button>Animations</button>
-                    </li>
-
-                    <li>
-                      <button>Bootstrap</button>
-                    </li>
-
-                  </ul>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-
-        <div className="tree">
-          <ul>
-            <li>
-              <button>Databases</button>
-              <ul>
-                <li>
-                  <button>NoSQL</button>
-                  <ul>
-                    <li>
-                      <button>MongoDB</button>
-                    </li>
-                  </ul>
-                </li>
-
-                <li>
-                  <button>SQL</button>
-                  <ul>
-                    <li>
-                      <button>MySQL</button>
-                    </li>
-                  </ul>
-                </li>
-                
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </div> {/* ROW */}
     </div>
+
+    <div className="row">
+      <div className="tree card">
+        <ul>
+          <li>
+            <button>Front-End</button>
+            <ul>
+
+              <li>
+                <button>JavaScript (ES6+)</button>
+
+                  <ul>
+                    <li>
+                      <button>React.js</button>
+                    </li>
+                  </ul>
+              </li>
+
+              <li>
+                <button>HTML5</button>
+
+                <ul>
+                  <li>
+                    <button>Canvas</button>
+                  </li>
+                </ul>
+
+              </li>
+
+              <li>
+                <button>CSS3</button>
+                <ul>
+
+                  <li>
+                    <button>Animations</button>
+                  </li>
+
+                  <li>
+                    <button>Bootstrap</button>
+                  </li>
+
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+
+      <div className="tree card">
+        <ul>
+          <li>
+            <button>Databases</button>
+            <ul>
+              <li>
+                <button>NoSQL</button>
+                <ul>
+                  <li>
+                    <button>MongoDB</button>
+                  </li>
+                </ul>
+              </li>
+
+              <li>
+                <button>SQL</button>
+                <ul>
+                  <li>
+                    <button>MySQL</button>
+                  </li>
+                </ul>
+              </li>
+              
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div> {/* ROW */}
+  </div>
 
   </Layout>
 );
