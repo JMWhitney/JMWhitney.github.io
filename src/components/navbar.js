c1306d0670bf1b07f8ccc88a3e4103519a0e0cf5
JMWhitney <JMWhitney96@gmail.com>
2019-06-05 23:38:39 -0700

    Title text is now rendered more consistently across any resolution size. Fixed a bug that prevented the current page icon from highlighting properly. Changed the way the navbar mobile hamburger menu is created. Added animation to the hamburger menu.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -22,53 +22,54 @@ class NavBar extends React.Component {
 
   render() { 
     return ( 
-      <nav id="navbar">
-      <Logo />
-
-      <div id="contact-icons">
-
-        <a href="https://github.com/JMWhitney">
-          <i className="fab fa-github"></i>
-        </a>
-
-        <a href="https://www.linkedin.com/in/justin-whitney-7b7796172/">
-          <i className="fab fa-linkedin-in"></i>
-        </a>
-
-      </div>
       
-      <div id="links">
+      <div id="nav-wrapper">
 
-        <Link onClick={ this.highlight } className="active" to="/">
-          <div id="triangle"></div>
-          <i className="fas fa-home"></i>
-        </Link>
+        <nav id="navbar">
 
-        <Link onClick={ this.highlight } to="/skills">
-          <div id="triangle"></div>
-          <i className="fas fa-toolbox"></i>
-        </Link>
+          <Logo />
 
-        <Link onClick={ this.highlight } to="/portfolio">
-          <div id="triangle"></div>
-          <i className="fas fa-image"></i>
-        </Link>
+          <div id="contact-icons">
 
-        <Link onClick={ this.highlight } to="/contact">
-          <div id="triangle"></div>
-          <i className="fas fa-envelope"></i>
-        </Link>
+            <a href="https://github.com/JMWhitney">
+              <i className="fab fa-github"></i>
+            </a>
 
+            <a href="https://www.linkedin.com/in/justin-whitney-7b7796172/">
+              <i className="fab fa-linkedin-in"></i>
+            </a>
+
+          </div>
+        
+          <div id="links">
+
+            <Link onClick={ this.highlight } className="active" to="/">
+              <i className="fas fa-home"></i>
+            </Link>
+
+            <Link onClick={ this.highlight } to="/skills">
+              <i className="fas fa-toolbox"></i>
+            </Link>
+
+            <Link onClick={ this.highlight } to="/portfolio">
+              <i className="fas fa-image"></i>
+            </Link>
+
+            <Link onClick={ this.highlight } to="/contact">
+              <i className="fas fa-envelope"></i>
+            </Link>
+
+          </div>
+        
+        </nav>
+
+        <div className="burger-wrapper">
+          <input className="burger-check" id="burger-check" type="checkbox"></input>
+          <label htmlFor="burger-check" className="burger"></label>
+        </div>
+        
       </div>
-      
-      <div id="menu">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      
-    </nav>
-   );
+    );
   }
 }
 
