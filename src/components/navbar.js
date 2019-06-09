1e116283de4d356fb03d277b62ca2671e6e0589b
JMWhitney <JMWhitney96@gmail.com>
2019-06-08 23:39:00 -0700

    Navbar is now responsive, properly hides on mobile devices, and drops down when the hamburger menu icon is clicked.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,75 +2,54 @@ import React from 'react';
 import Logo from './logo';
 import { Link } from "gatsby";
 
-class NavBar extends React.Component {
+const NavBar = () => ( 
+  <div id="nav-wrapper">
 
-  componentDidMount() {
-    this.setState(() => {
-      return {links: document.getElementById("links")};
-    })
-  }
+    <input className="burger-check" id="burger-check" type="checkbox"></input>
+    <label htmlFor="burger-check" className="burger"></label>
 
-  highlight = (event) => {
-    // Clear previous effect
-    this.state.links.childNodes.forEach((element) => {
-      if(element.className === "active") element.className = "";
-    })
-  
-    //Set effect on target
-    event.target.parentElement.className = "active";
-  }
 
-  render() { 
-    return ( 
-      
-      <div id="nav-wrapper">
+    <nav id="navbar">
 
-        <nav id="navbar">
+      <Logo />
+      <div className="placeholder"></div>
 
-          <Logo />
+      <div id="contact-icons">
 
-          <div id="contact-icons">
+        <a href="https://github.com/JMWhitney">
+          <i className="fab fa-github"></i>
+        </a>
 
-            <a href="https://github.com/JMWhitney">
-              <i className="fab fa-github"></i>
-            </a>
+        <a href="https://www.linkedin.com/in/justin-whitney-7b7796172/">
+          <i className="fab fa-linkedin-in"></i>
+        </a>
 
-            <a href="https://www.linkedin.com/in/justin-whitney-7b7796172/">
-              <i className="fab fa-linkedin-in"></i>
-            </a>
-
-          </div>
-        
-          <div id="links">
-
-            <Link onClick={ this.highlight } className="active" to="/">
-              <i className="fas fa-home"></i>
-            </Link>
-
-            <Link onClick={ this.highlight } to="/skills">
-              <i className="fas fa-toolbox"></i>
-            </Link>
-
-            <Link onClick={ this.highlight } to="/portfolio">
-              <i className="fas fa-image"></i>
-            </Link>
-
-            <Link onClick={ this.highlight } to="/contact">
-              <i className="fas fa-envelope"></i>
-            </Link>
-
-          </div>
-        
-        </nav>
-
-        <div className="burger-wrapper">
-          <input className="burger-check" id="burger-check" type="checkbox"></input>
-          <label htmlFor="burger-check" className="burger"></label>
-        </div>
-        
       </div>
-    );
-  }
-}
+    
+      <div id="links">
+
+        <Link className="active" to="/">
+          <i className="fas fa-home"></i>
+        </Link>
+
+        <Link to="/skills">
+          <i className="fas fa-toolbox"></i>
+        </Link>
+
+        <Link to="/portfolio">
+          <i className="fas fa-image"></i>
+        </Link>
+
+        <Link to="/contact">
+          <i className="fas fa-envelope"></i>
+        </Link>
+
+      </div>
+    
+    </nav>
+    
+    
+  </div>
+);
 
 export default NavBar;
