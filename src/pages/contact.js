3f8cabec1357c9203164516d3fe929cab6825d9f
JMWhitney <JMWhitney96@gmail.com>
2019-06-12 23:41:50 -0700

    Slight refactoring of many components. Major restyling of contact form.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,74 +6,50 @@ const Contact = () => (
   <Layout>
     <SEO title="Justin Whitney" />
 
-    <div id="background3">
+    <div id="background">
+      <div className="grid-wrapper">
         <div className="grid">
-
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-
+          <div className="vertical">
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+          </div>
+          <div className="horizontal">
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+          </div>
         </div>
-
       </div>
-
-      <div className="dots-horizontal">
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-      </div>
-
-      <div className="dots-verticle">
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-      </div>
+    </div>
     
-    <form className="card" id="contact" action="https://formspree.io/JMWhitney96@gmail.com" method="POST">
+    <form id="contact" action="https://formspree.io/JMWhitney96@gmail.com" method="POST">
       <h1>Send Me a Message</h1>
-      <input type="email" name="email" placeholder="Your email"></input>
+      <div className="field-half">
+        <input type="text" name="name" placeholder="Your Name"></input>
+        <input type="email" name="email" placeholder="Your email"></input>
+      </div>
       <textarea name="message" placeholder="Message"></textarea>
-      <button type="submit">Send</button>
+      <div className="field-half">
+        <button type="submit">Send</button>
+        <button type="reset">Clear</button>
+      </div>
     </form>
 
   </Layout>
